ea3bbf02b9964d803eab72ab82f6396ecb69fc90
mbrandonw <mbw234@gmail.com>
2014-01-10 17:22:44 -0500

    Small changes to get it inline with my setup.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,15 @@
 alias reload!='. ~/.zshrc'
+
+# Easy cd into certain projects.
+alias dotfiles='cd ~/Projects/dotfiles'
+alias kickstarter='cd ~/Projects/kickstarter'
+alias kickstarter_ios='cd ~/Projects/kickstarter_ios'
+
+# List files nicely
+alias ll='ls -l'
+
+# Easy way to get ip address
+alias myip="curl -s http://checkip.dyndns.org | sed 's/[a-zA-Z/<> :]//g'"
+
+# Bundle exec shortcut
+alias be='bundle exec'
