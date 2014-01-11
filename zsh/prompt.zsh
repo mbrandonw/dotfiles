82bb51cc264aeb706da79443d9a628b1bdf25dde
mbrandonw <mbw234@gmail.com>
2014-01-10 19:01:42 -0500

    Small updates.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -72,12 +72,12 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'$(directory_name) $(git_dirty)$(need_push)› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
 
 precmd() {
-  title "zsh" "%m" "%55<...<%~"
+  title "%m" "%55<...<%~"
   set_prompt
 }
