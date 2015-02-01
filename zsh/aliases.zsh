66540470785f62210cdc4d776a6f95cda85e4b0a
Brandon Williams <mbw234@gmail.com>
2015-02-01 11:12:32 -0500

    Added some aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,11 @@ alias reload!='. ~/.zshrc'
 
 # Easy cd into certain projects.
 alias dotfiles='cd ~/.dotfiles'
+alias fewbutripe='cd ~/projects/mbrandonw.github.io'
+
+# cd into kickstarter projects
 alias kickstarter='cd ~/projects/kickstarter-web'
+alias rosie='cd ~/projects.rosie'
 alias kickstarter_ios='cd ~/projects/kickstarter-ios'
 
 # List files nicely
